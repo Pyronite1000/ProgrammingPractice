94f5a0c77029e4a37f9837d11e0be6f8ab537161
Pyronite1000 <ammercer97@gmail.com>
2018-07-29 15:35:25 -0400

    Updating Changes

---
+++ ProgrammingPractice/OperatorsAndExpressions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAndExpressions
+{
+    class Program
+    {
+        static void ArithmaticOperators(string[] args)
+        {
+            var A = 10;
+            var B = 20;
+
+            Console.WriteLine("A" + "B");
+            Console.ReadLine();
+
+            int addition = A + B;
+            int subtraction = A - B;
+            int multplication = A * B;
+            int division = B / A;
+            int remainder = B % A;
+            int addOne = A++;
+            int subtractOne = A--;
+        }
+
+        static void RelationalOperators(string[] args)
+        {
+
+        }
+    }
+}
