ed6ce80d85eb03fae74b37082d37981f1598070f
Pyronite1000 <ammercer97@gmail.com>
2018-08-22 12:29:50 -0400

    Initial commit

--- GameReview/GameReview/Properties/AssemblyInfo.cs
+++ GameReview/GameReview/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d639c2bd-1233-405f-9d16-fff1ba924793")]
+[assembly: Guid("d0ccb7d5-b86e-4b4d-b017-eb985a1ad67d")]
 
 // Version information for an assembly consists of the following four values:
 //
