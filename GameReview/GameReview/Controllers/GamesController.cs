bf26996de6bcf790adba4f217230e5333d0a74a8
Pyronite1000 <ammercer97@gmail.com>
2018-09-11 10:29:22 -0400

    Basics and Framework

---
+++ GameReview/GameReview/Controllers/GamesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameReview.Models;
+
+namespace GameReview.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Random()
+        {
+            var game = new Game() { Name = "Black Ops 2" };
+
+            return View(game);
+        }
+    }
+}
